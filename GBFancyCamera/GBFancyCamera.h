66449fca1d239abf6906e874c2576ba8fed92736
Luka Mirosevic <luka@goonbee.com>
2013-08-29 18:23:56 +0200

    Initial Commit

---
+++ GBFancyCamera/GBFancyCamera.h
@@ -0,0 +1,13 @@
+//
+//  GBFancyCamera.h
+//  GBFancyCamera
+//
+//  Created by Luka Mirosevic on 29/08/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBFancyCamera : NSObject
+
+@end
---
+++ GBFancyCamera/GBFancyCamera.m
@@ -0,0 +1,13 @@
+//
+//  GBFancyCamera.m
+//  GBFancyCamera
+//
+//  Created by Luka Mirosevic on 29/08/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import "GBFancyCamera.h"
+
+@implementation GBFancyCamera
+
+@end
